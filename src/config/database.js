da95ec4ca129c3613460116de37caa7efc451242
Fabricio Andrade <131885041+andradejs@users.noreply.github.com>
2024-06-07 21:28:48 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,27 @@
 // como nome, linguagem, host, senha, nome de usuario, campos com datas.
 
 
+
+
 require('dotenv').config()
 
 module.exports = {
 
-  dialect: 'mariadb',
+  dialect: 'mysql',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database:process.env.DATABASE,
+  // pool: {
+  //   max: 5, // número máximo de conexões no pool
+  //   min: 0, // número mínimo de conexões no pool
+  //   acquire: 30000, // tempo máximo, em milissegundos, que o pool tentará obter uma conexão antes de lançar um erro
+  //   idle: 10000, // tempo máximo, em milissegundos, que uma conexão pode ficar ociosa antes de ser liberada
+  // },
+  dialectOptions: {
+    connectTimeout: 60000 // tempo máximo de conexão, em milissegundos
+  },
   define: {
     timestamps: true,
     underscored: true,
@@ -23,4 +34,5 @@ module.exports = {
     timezone: 'America/Sao_Paulo',
   },
   timezone: 'America/Sao_Paulo',
+
 }
